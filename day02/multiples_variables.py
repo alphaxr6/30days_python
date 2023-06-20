16a4d3fc5368e7c7773dfac98b8862f098febdf0
Thomas <thomasmarty94@hotmail.Fr>
2023-06-20 09:07:19 +0200

    push multiples variables files

---
+++ day02/multiples_variables.py
@@ -0,0 +1,8 @@
+first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Sweden', 250, True
+
+print(first_name, last_name, country, age, is_married)
+print('First Name: ', first_name)
+print('Last Name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
