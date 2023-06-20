2598680da2361e6d1750364192d720f51aaffa8d
Thomas <thomasmarty94@hotmail.Fr>
2023-06-20 08:57:57 +0200

    add variable.py file

---
+++ day02/variables.py
@@ -0,0 +1,25 @@
+# Variables in Python
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+country = 'Finland'
+city = 'Helsinki'
+age = 250
+is_married = True
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+person_info = {
+   'firstname':'Asabeneh',
+   'lastname':'Yetayeh',
+   'country':'Finland',
+   'city':'Helsinki'
+   }
+
+print('First Name:', first_name)
+print('First name length:', len(first_name))
+print('Last name:', last_name)
+print('Last Name lenght:', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age:', age)
+print('Married:', is_married)
+print('SKills', skills)
+print('Person Information:', person_info)
