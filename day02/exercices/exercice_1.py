a9a64e6d5e6e9bf23b52144a2139b69d6c530360
Thomas <thomasmarty94@hotmail.Fr>
2023-06-20 09:49:19 +0200

    update exercices 1 python file

--- day02/exercices/exercice_1.py
+++ day02/exercices/exercice_1.py
@@ -12,3 +12,20 @@ is_true = True
 is_light_on = False
 
 second_name, last_second_name, second_age, second_is_married = 'Jean', 'martin', 19, True
+
+print(first_name)
+print(last_name)
+print(full_name)
+print(country)
+print(city)
+print(age)
+print(year)
+print(is_married)
+print(is_true)
+print(is_light_on)
+
+
+print(second_name)
+print(last_second_name)
+print(second_age)
+print(second_is_married)
