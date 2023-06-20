e29d34a7d09790754b71ffbf158811fc57374a95
Thomas <thomasmarty94@hotmail.Fr>
2023-06-20 09:47:40 +0200

    add exercices 1

---
+++ day02/exercices/exercice_1.py
@@ -0,0 +1,14 @@
+print('Day 2: 30 Days of python programming')
+
+first_name = 'Thomas'
+last_name = 'Dupont'
+full_name = 'Thomas Dupont'
+country = 'Scotland'
+city = 'Aberdeen'
+age = 29
+year = 2023
+is_married = False
+is_true = True
+is_light_on = False
+
+second_name, last_second_name, second_age, second_is_married = 'Jean', 'martin', 19, True
