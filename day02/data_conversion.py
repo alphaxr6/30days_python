668dd01e2dfbb4c95fe5c9b95b18156d4329ccf9
Thomas <thomasmarty94@hotmail.Fr>
2023-06-20 09:29:52 +0200

    add data_conversion.py file

---
+++ day02/data_conversion.py
@@ -0,0 +1,25 @@
+num_int = 10
+print('num_int', num_int)
+num_float = float(num_int)
+print('num_float:', num_float)
+print(type(num_float))
+
+gravity = 9.81
+print(int(gravity))
+print(type(int(gravity)))
+
+num_int = 10
+print(num_int)
+num_str = str(num_int)
+print(num_str)
+print(type(num_str))
+
+num_str = '10.4'
+print('num_int', int(num_str))
+print('num_float', float(num_str))
+
+first_name = 'Asabeneh'
+print(first_name)
+first_name_to_list = list(first_name)
+print(first_name_to_list)
+print(type(first_name_to_list))
