b57cd7f2ae1f82d1ec56bb3ea239109b892a69b5
Thomas <thomasmarty94@hotmail.Fr>
2023-06-21 08:49:00 +0200

    add the circle.py file

---
+++ day03/circle.py
@@ -0,0 +1,21 @@
+radius = 10 
+area_of_circle = 3.14 * radius * 2
+print('Area of Circle: ', area_of_circle)
+
+#Area of rectangle
+lenght = 10
+width = 20
+area_of_rectangle = lenght * width
+print('Area of rectangle: ', area_of_rectangle)
+
+#Weight of an object
+mass = 75
+gravity = 9.81
+weight = mass * gravity
+print(weight, 'N')
+
+#Density of a liquid
+mass = 75
+volume = 0.075
+density = mass / volume
+print('Density: ', density)
