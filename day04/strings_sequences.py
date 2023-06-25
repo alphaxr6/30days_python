a6593c7013591437500f7a3cf7d097d54c908be7
Thomas <thomasmarty94@hotmail.Fr>
2023-06-25 21:58:16 +0200

    add python strings exercices day 04

---
+++ day04/strings_sequences.py
@@ -0,0 +1,22 @@
+language = 'Python'
+a,b,c,d,e,f = language
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
+first_letter = language[0]
+print(first_letter)
+second_letter = language[1]
+print(second_letter)
+last_index = len(language) - 1
+last_letter = language[last_index]
+print(last_letter)
+
+last_letter = language[-1]
+print(last_letter)
+second_last = language[-2]
+print(second_last)
+
