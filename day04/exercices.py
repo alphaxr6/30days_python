815b8f3d561a4ecb9a19191f6d50360b6b472de1
Thomas <thomasmarty94@hotmail.Fr>
2023-06-28 23:15:21 +0200

    add exercices.py file

---
+++ day04/exercices.py
@@ -0,0 +1,7 @@
+concatenation = ['Thirty', 'Days', 'Of', 'Python']
+result = ' '.join(concatenation)
+print(result)
+
+concatenation_2 = ['Coding', 'For', 'All']
+result_2 = ' '.join(concatenation_2)
+print(result_2)
