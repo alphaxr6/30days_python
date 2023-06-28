5f4bff10477900f501776806e68cc08f86f769d9
Thomas <thomasmarty94@hotmail.Fr>
2023-06-28 23:28:52 +0200

    string exercices

--- day04/exercices.py
+++ day04/exercices.py
@@ -1,7 +1,33 @@
+print("1")
 concatenation = ['Thirty', 'Days', 'Of', 'Python']
 result = ' '.join(concatenation)
 print(result)
 
+print("2")
 concatenation_2 = ['Coding', 'For', 'All']
 result_2 = ' '.join(concatenation_2)
 print(result_2)
+
+print("3")
+company = 'Coding For All'
+print(company)
+print(len(company))
+print(company.upper())
+lower = company.lower()
+print(lower)
+
+print("8")
+print(lower.capitalize())
+print(lower.swapcase())
+print(lower.title())
+
+print("9")
+company = 'Coding For All'
+slice = company[0:5]
+print(slice)
+
+print("10")
+print(company.find('Coding'))
+
+print("11")
+print(company.replace('Coding', 'Python'))
