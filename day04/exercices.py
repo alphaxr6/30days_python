54746f5015281fb5f188ac9680de7f96a5c46ed3
Thomas <thomasmarty94@hotmail.Fr>
2023-07-31 21:01:13 +0200

    finish day 04 exercices

--- day04/exercices.py
+++ day04/exercices.py
@@ -31,3 +31,51 @@ print(company.find('Coding'))
 
 print("11")
 print(company.replace('Coding', 'Python'))
+
+print("12")
+company = "Python for everyone"
+print(company.replace('everyone', 'all'))
+
+print('13')
+print(company.split())
+
+print('14')
+gafam = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(gafam.split(', '))
+
+print('15')
+company = "Coding for all"
+print(company[0])
+
+print('16')
+print(company[-1])
+
+print('17')
+print(company[10])
+
+print('18')
+print(company[0], company[6], company[10])
+
+print('19')
+company = 'Coding For all'
+print(company[0], company[6], company[10])
+
+print('20')
+print(company.find('C'))
+
+print('21')
+print(company.find('F'))
+
+print('22')
+print(company.rfind('l'))
+
+print('23')
+sentence = "You cannot end a sentence with because because because is a conjunction"
+print(sentence.find("because"))
+
+print('24')
+print(sentence.rindex('because'))
+
+print('28')
+code = "Coding for All"
+print()
