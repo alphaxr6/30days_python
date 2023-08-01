f73ea2a307e03f67680451a47b1aa89abbb6360d
Thomas <thomasmarty94@hotmail.Fr>
2023-08-01 23:36:39 +0200

    add modifying list

---
+++ day05/modifying_list.py
@@ -0,0 +1,11 @@
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits[0] = 'avocado'
+print(fruits)
+
+fruits[1] = 'apple'
+print(fruits)
+
+last_index = len(fruits) - 1
+fruits[last_index] = 'lime'
+
+print(fruits)
