68cc24fdc81abfc5373ce447001650a268cc98ab
Thomas <thomasmarty94@hotmail.Fr>
2023-08-01 23:17:45 +0200

    create positive_indexing.py

---
+++ day05/positive_indexing.py
@@ -0,0 +1,13 @@
+fruits = ['banana', 'orange', 'mango', 'lemon']
+first_fruits = fruits[0]
+print(first_fruits)
+
+second_fruits = fruits[1]
+print(second_fruits)
+
+last_fruits = fruits[3]
+print(last_fruits)
+
+last_index = len(fruits) - 1
+last_fruits = fruits[last_index]
+print(last_fruits)
