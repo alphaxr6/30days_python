3bf385aa6d8adedfe3f563826e00641c939fa02e
Thomas <thomasmarty94@hotmail.Fr>
2023-08-01 23:12:40 +0200

    create introduction file

---
+++ day05/introduction.py
@@ -0,0 +1,26 @@
+lst = list()
+
+empty_list = list()
+print(len(empty_list))
+
+lst = []
+
+empty_list = []
+print(len(empty_list))
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+animal_products = ['milk', 'meat', 'butter', 'yoghurt']
+web_techs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongoDB']
+countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
+
+print('Fruits:', fruits)
+print('Number of fruits:', len(fruits))
+print('Vegetables:', vegetables)
+print('Number of vegetables:', len(vegetables))
+print('Animal Products:', animal_products)
+print('Number of Animal Products:', len(animal_products))
+print('Web technologies:', web_techs)
+print('Number of web tech:', len(web_techs))
+print('Countries:', countries)
+print('Number of countries:', len(countries))
