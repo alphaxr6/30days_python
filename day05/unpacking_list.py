8f4866b1600a95bb741cc116aa275c1092a691b9
Thomas <thomasmarty94@hotmail.Fr>
2023-08-01 23:27:20 +0200

    test unpacking list python

--- day05/unpacking_list.py
+++ day05/unpacking_list.py
@@ -12,10 +12,11 @@ print(second_fruit)
 print(thrid_fruit)
 print(rest)
 
-first, second, third, *rest, tenth = [1,2,3,4,5,6,7,8,9,10]
+first, second, third, test, *rest, tenth = [1,2,3,4,5,6,7,8,9,10]
 print(first)
 print(second)
 print(third)
+print(test)
 print(rest)
 print(tenth)
 
