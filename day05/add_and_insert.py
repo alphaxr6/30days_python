196545f04d9215b554d48fde5beeaf662790d56f
Thomas <thomasmarty94@hotmail.Fr>
2023-08-01 23:42:57 +0200

    add add and insert file

---
+++ day05/add_and_insert.py
@@ -0,0 +1,15 @@
+#syntax
+
+lst = list()
+lst.append('item')
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.append('apple')
+print(fruits)
+fruits.append('lime')
+print(fruits)
+
+fruits.insert(2, 'apple')
+print(fruits)
+fruits.insert(3, 'lime')
+print(fruits)
