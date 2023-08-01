58372b6615d8742f0fd54db0cb8355f4eaee3a57
Thomas <thomasmarty94@hotmail.Fr>
2023-08-01 23:19:32 +0200

    creating negative indexing

---
+++ day05/negative_indexing.py
@@ -0,0 +1,8 @@
+fruits = ['banana', 'orange', 'mango', 'lemon']
+first_fruits = fruits[-4]
+last_fruits = fruits[-1]
+second_last = fruits[-2]
+
+print(first_fruits)
+print(last_fruits)
+print(second_last)
