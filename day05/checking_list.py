05657c349e8512122bf1322ba9b262f32c5444c6
Thomas <thomasmarty94@hotmail.Fr>
2023-08-01 23:39:40 +0200

    udpate chcking list

--- day05/checking_list.py
+++ day05/checking_list.py
@@ -1,5 +1,5 @@
 fruits = ['banana', 'orange', 'mango', 'lemon']
 does_exist = 'banana' in fruits
 print(does_exist)
-doest_exist = 'lime' in fruits 
+does_exist = 'lime' in fruits 
 print(does_exist)
