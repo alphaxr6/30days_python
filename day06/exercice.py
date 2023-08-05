abb8381cbc287e6f283bc6f3d79c96e02bd60e9b
Thomas <thomasmarty94@hotmail.Fr>
2023-08-05 19:59:28 +0200

    test

--- day06/exercice.py
+++ day06/exercice.py
@@ -22,7 +22,7 @@ print(type(siblings))
 print('6')
 print(len(siblings))
 last_index = len(siblings) - 1
-before = last_index - 2
+before = last_index - 1
 
 parents = siblings[before:last_index]
 print(parents)
