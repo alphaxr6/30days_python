0c6211811e60713b838056b028346216d460ba46
Thomas <thomasmarty94@hotmail.Fr>
2023-08-07 17:03:10 +0200

    update set.py

--- day07/set.py
+++ day07/set.py
@@ -80,34 +80,37 @@ even_numbers = {0, 2, 4, 6, 8, 10}
 whole_numbers.issubset(even_numbers)
 whole_numbers.issuperset(even_numbers)
 
+print(whole_numbers)
+
 #checking difference between 2 sets
 whole_numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 even_numbers = {0, 2, 4, 6, 8, 10}
 whole_numbers.difference(even_numbers)
-
+print(whole_numbers)
 
 python = {'p', 'y', 't', 'o','n'}
 dragon = {'d', 'r', 'a', 'g', 'o','n'}
 python.difference(dragon)
-
+print(python)
 #Symmetric
 
 whole_numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 some_numbers = {1, 2, 3, 4, 5}
 whole_numbers.symmetric_difference(some_numbers)
-
+print(whole_numbers)
 python = {'p', 'y', 't', 'h', 'o','n'}
 dragon = {'d', 'r', 'a', 'g', 'o','n'}
 python.symmetric_difference(dragon)
-
+print(python)
 
 #Join and disjoin
 
 even_numbers = {0,2,4,6,8}
 odd_numbers = {1,3,5,7,9}
 even_numbers.isdisjoint(odd_numbers)
-
+print(even_numbers)
 python = {'p', 'y', 't', 'h', 'o','n'}
 dragon = {'d', 'r', 'a', 'g', 'o','n'}
 python.isdisjoint(dragon)
+print(python)
 
