6a9caa59611cb5dbfe31c4d6d8c5cc91b7ff2721
Thomas <thomasmarty94@hotmail.Fr>
2023-08-07 17:14:31 +0200

    update exercices 2

--- day07/exercices.py
+++ day07/exercices.py
@@ -3,6 +3,7 @@ A = {19, 22, 24, 20, 25, 26}
 B = {19, 22, 20, 25, 26, 24, 28, 27}
 age = [22, 19, 24, 25, 26, 24, 25, 24]
 
+print('Exercice 1')
 print('1')
 print(len(it_companies))
 
@@ -22,3 +23,34 @@ print(it_companies)
 print('5')
 it_companies.discard('IBM')
 print(it_companies)
+
+print('Exercice 2')
+print('1')
+A.join(B)
+print(A)
+
+print('2')
+inter = A.intersection(B)
+print(inter)
+
+print('3')
+subset = A.issubset(B)
+print(subset)
+
+print('4')
+disjoin = A.isdisjoint(B)
+print(disjoin)
+
+print('5')
+A_and_B = A.union(B)
+B_and_A = B.union(A)
+print(A_and_B)
+print(B_and_A)
+
+print('6')
+symmetric = A.symmetric_difference(B)
+print(symmetric)
+
+print('7')
+del A
+del B
